2284a74dcab0e185583f06f14187ef894c6af8fd
ikeda-ks <kosuke.ikeda@cleaveware.com>
2023-08-30 13:15:24 +0900

    update create_symbolic_link

--- scripts/create_symbolic_link.sh
+++ scripts/create_symbolic_link.sh
@@ -13,14 +13,14 @@ ln -snfv "$(pwd)/_git" "$HOME/_git"
 # keybinds for vscode
 vsc_keybindings="keybindings.json"
 # for windows
-ln -snfv "${PWD%/*}/etc/$vsc_keybindings" "$HOME/AppData/Roaming/Code/User/$vsc_keybindings"
+ln -snfv "$HOME/dotfiles/etc/$vsc_keybindings" "$HOME/AppData/Roaming/Code/User/$vsc_keybindings"
 # for mac
-ln -snfv "${PWD%/*}/etc/$vsc_keybindings" "$HOME/Library/Application Support/Code/User/$vsc_keybindings"
+ln -snfv "$HOME/dotfiles/etc/$vsc_keybindings" "$HOME/Library/Application Support/Code/User/$vsc_keybindings"
 
 # iterm config
-cp -f iTerm2/com.googlecode.iterm2.plist ~/Library/Preferences
+cp -f "$HOME/dotfiles/etc/com.googlecode.iterm2.plist" ~/Library/Preferences
 
 # starship config
-ln -snfv "${PWD%/*}/etc/starship.toml" "$HOME/.config/starship.toml"
+ln -snfv "$HOME/dotfiles/etc/starship.toml" "$HOME/.config/starship.toml"
 
 echo "create link completed!!!!"
