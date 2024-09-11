8a3cb0d7dfd18b8177110c453b0e4a8d342bd255
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-09-12 02:39:07 +0900

    fix export

--- .zsh.d/export.zsh
+++ .zsh.d/export.zsh
@@ -1,12 +1,12 @@
-export DOCKER_HOST="unix://$XDG_CONFIG_HOME/colima/default/docker.sock"
+export XDG_CONFIG_HOME="$HOME/.config" # change lazygit config directory
+export POETRY_ROOT="$HOME/.local/"
 export LANG="en_us.UTF-8"
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
-export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$POETRY_ROOT/bin:$PATH"
-export POETRY_ROOT="$HOME/.local/"
-export PYENV_ROOT="$HOME/.pyenv"
-export XDG_CONFIG_HOME="$HOME/.config" # change lazygit config directory
+
+DOCKER_SOCKET_PATH="$XDG_CONFIG_HOME/colima/default/docker.sock"
+export DOCKER_HOST="unix://$DOCKER_SOCKET_PATH"
 
 if [ -f "$HOME/openai_api_key" ]; then
     export OPENAI_API_KEY=$(cat "$HOME/openai_api_key")
