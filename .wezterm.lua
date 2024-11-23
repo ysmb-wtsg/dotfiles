61e789df7c6a04349595ad9068c7a0d7e9fa355b
ikeda-ks <kosuke.ikeda@cleaveware.com>
2024-11-23 22:25:27 +0900

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,10 +10,29 @@ config.initial_cols = 120
 config.initial_rows = 32
 
 config.window_decorations = "RESIZE"
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+	return {
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+	}
+end)
 
 config.color_scheme = "Ayu Dark (Gogh)"
 
-config.font_size = 15
+config.font_size = 13
 
 local act = wezterm.action
 config.keys = {
