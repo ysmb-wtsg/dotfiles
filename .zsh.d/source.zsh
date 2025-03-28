37a31d597436241454890e3e3f625046e1611d50
ikeda-ks <kosuke.ikeda@cleaveware.com>
2025-03-28 11:23:15 +0900

    fix asdf source

--- .zsh.d/source.zsh
+++ .zsh.d/source.zsh
@@ -1,3 +1,3 @@
-[ -s "/opt/homebrew/opt/asdf/libexec/asdf.sh" ] && source /opt/homebrew/opt/asdf/libexec/asdf.sh
+[ -s "$HOME/.asdf/asdf.sh" ] && source "$HOME/.asdf/asdf.sh"
 
 [ -s "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ] && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
