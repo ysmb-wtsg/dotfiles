49f0faa362ab97ca7535ea1be0298270a9605d7e
ysmb-wtsg <telecaster1121@gmail.com>
2026-01-10 10:32:33 +0900

    format

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 config.initial_cols = 120
@@ -13,125 +13,125 @@ config.window_decorations = "RESIZE"
 config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local background = "#5c6d74"
-	local foreground = "#FFFFFF"
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
 
-	if tab.is_active then
-		background = "#ae8b2d"
-		foreground = "#FFFFFF"
-	end
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
 
-	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
 
-	return {
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-	}
+  return {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+  }
 end)
 
 config.color_scheme = "Ayu Dark (Gogh)"
 
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono", weight = "ExtraBold" },
-	{ family = "ヒラギノ角ゴシック", weight = "Bold" },
+  { family = "JetBrains Mono", weight = "ExtraBold" },
+  { family = "ヒラギノ角ゴシック", weight = "Bold" },
 })
 config.font_size = 13
 
 local act = wezterm.action
 config.keys = {
-	{
-		key = "d",
-		mods = "CMD",
-		action = act.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "f",
-		mods = "CMD",
-		action = act.ToggleFullScreen,
-	},
-	{
-		key = "h",
-		mods = "CMD",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "h",
-		mods = "CMD|SHIFT",
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		key = "h",
-		mods = "CTRL|CMD",
-		action = act.SendKey({ key = "LeftArrow" }),
-	},
-	{
-		key = "j",
-		mods = "CMD",
-		action = act({ ScrollByLine = 1 }),
-	},
-	{
-		key = "j",
-		mods = "CMD|SHIFT",
-		action = act({ ScrollByPage = 1 }),
-	},
-	{
-		key = "j",
-		mods = "CTRL|CMD",
-		action = act.SendKey({ key = "DownArrow" }),
-	},
-	{
-		key = "k",
-		mods = "CMD",
-		action = act({ ScrollByLine = -1 }),
-	},
-	{
-		key = "k",
-		mods = "CMD|SHIFT",
-		action = act({ ScrollByPage = -1 }),
-	},
-	{
-		key = "k",
-		mods = "CTRL|CMD",
-		action = act.SendKey({ key = "UpArrow" }),
-	},
-	{
-		key = "l",
-		mods = "CMD",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "l",
-		mods = "CTRL|CMD",
-		action = act.SendKey({ key = "RightArrow" }),
-	},
-	{
-		key = "l",
-		mods = "CMD|SHIFT",
-		action = act.ActivateTabRelative(1),
-	},
-	{
-		key = "p",
-		mods = "CMD",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "t",
-		mods = "CMD",
-		action = act.SpawnCommandInNewTab({
-			cwd = wezterm.home_dir,
-		}),
-	},
-	{
-		key = "w",
-		mods = "CMD",
-		action = act.CloseCurrentTab({ confirm = false }),
-	},
-	{
-		key = "z",
-		mods = "CMD",
-		action = act.TogglePaneZoomState,
-	},
+  {
+    key = "d",
+    mods = "CMD",
+    action = act.CloseCurrentPane({ confirm = true }),
+  },
+  {
+    key = "f",
+    mods = "CMD",
+    action = act.ToggleFullScreen,
+  },
+  {
+    key = "h",
+    mods = "CMD",
+    action = act.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "h",
+    mods = "CMD|SHIFT",
+    action = act.ActivateTabRelative(-1),
+  },
+  {
+    key = "h",
+    mods = "CTRL|CMD",
+    action = act.SendKey({ key = "LeftArrow" }),
+  },
+  {
+    key = "j",
+    mods = "CMD",
+    action = act({ ScrollByLine = 1 }),
+  },
+  {
+    key = "j",
+    mods = "CMD|SHIFT",
+    action = act({ ScrollByPage = 1 }),
+  },
+  {
+    key = "j",
+    mods = "CTRL|CMD",
+    action = act.SendKey({ key = "DownArrow" }),
+  },
+  {
+    key = "k",
+    mods = "CMD",
+    action = act({ ScrollByLine = -1 }),
+  },
+  {
+    key = "k",
+    mods = "CMD|SHIFT",
+    action = act({ ScrollByPage = -1 }),
+  },
+  {
+    key = "k",
+    mods = "CTRL|CMD",
+    action = act.SendKey({ key = "UpArrow" }),
+  },
+  {
+    key = "l",
+    mods = "CMD",
+    action = act.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "l",
+    mods = "CTRL|CMD",
+    action = act.SendKey({ key = "RightArrow" }),
+  },
+  {
+    key = "l",
+    mods = "CMD|SHIFT",
+    action = act.ActivateTabRelative(1),
+  },
+  {
+    key = "p",
+    mods = "CMD",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "t",
+    mods = "CMD",
+    action = act.SpawnCommandInNewTab({
+      cwd = wezterm.home_dir,
+    }),
+  },
+  {
+    key = "w",
+    mods = "CMD",
+    action = act.CloseCurrentTab({ confirm = false }),
+  },
+  {
+    key = "z",
+    mods = "CMD",
+    action = act.TogglePaneZoomState,
+  },
 }
 
 return config
--- .zsh.d/export.zsh
+++ .zsh.d/export.zsh
@@ -1,10 +1,12 @@
 export XDG_CONFIG_HOME="$HOME/.config" # change lazygit config directory
 export DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 export POETRY_ROOT="$HOME/.local/"
+export ANDROID_HOME="/Users/user/Library/Android/sdk"
 export LANG="en_us.UTF-8"
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="$POETRY_ROOT/bin:$PATH"
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+export PATH="$ANDROID_HOME/bin:$PATH"
 
 DOCKER_SOCKET_PATH="$XDG_CONFIG_HOME/colima/default/docker.sock"
 export DOCKER_HOST="unix://$DOCKER_SOCKET_PATH"
