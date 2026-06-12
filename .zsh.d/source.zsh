7a8d864ec64c604cb69559b67b39db994d92ad37
ysmb-wtsg <telecaster1121@gmail.com>
2026-06-12 22:31:56 +0900

    update source.zsh

--- .zsh.d/source.zsh
+++ .zsh.d/source.zsh
@@ -1,3 +1,7 @@
 source "$HOME/.local/bin/env"
 
 [ -s "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ] && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+if [ -f '/Users/user/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/user/google-cloud-sdk/path.zsh.inc'; fi
+
+if [ -f '/Users/user/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/user/google-cloud-sdk/completion.zsh.inc'; fi
